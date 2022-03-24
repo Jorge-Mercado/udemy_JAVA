680b993ada0d6a1cf000571353f46d1d721879c2
Jorge Mercado <mercadoruizjorgearturo@gmail.com>
2022-03-23 22:33:30 -0600

    first commit

---
+++ src/basico/DatosPrimitivos.java
@@ -0,0 +1,16 @@
+package basico;
+import java.util.*;
+
+public class DatosPrimitivos {
+
+	public static void main(String[] args) {
+		/**
+		 * tipos primitivos enteros: byte,short,int,long y char.
+		 */
+		
+		byte numeroByte = 10; 
+		System.out.println("valor minimo del byte: " + Byte.MIN_VALUE);
+        System.out.println("valor maximo byte: " + Byte.MAX_VALUE);
+	}
+
+}
---
+++ src/basico/TiposdeDatos.java
@@ -0,0 +1,18 @@
+package basico;
+
+
+
+public class TiposdeDatos {
+
+	public static void main(String[] args) {
+		
+		var entero = 10;
+		String saludito = " adios"; // reglas: primer caracter es minuscula, no empezar con numeros, no caracteres
+		// especiales excepto el guion bajo, no usar acentos. 
+		var cadena2 = "nuevo saludo"; // inferencia de tipos de datos a partir de java jdk 10 
+		System.out.println(entero);
+		System.out.println(saludito);
+		System.out.println(cadena2);
+	}
+
+}
