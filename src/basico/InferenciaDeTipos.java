635b08f0dfae41fc1d78d61b7131da381dbe6051
Jorge Mercado <mercadoruizjorgearturo@gmail.com>
2022-03-29 19:37:29 -0600

    se agrega ejercicio de inferencia de tipos con var. 29 marzo 2022

---
+++ src/basico/InferenciaDeTipos.java
@@ -0,0 +1,16 @@
+package basico;
+
+public class InferenciaDeTipos {
+
+	public static void main(String[] args) {
+	
+
+		var numeroEntero = 10;
+		System.out.println("numeroEntero:" + numeroEntero);
+		// vamos a usar el modo debug en java por medio de break points
+		var numeroDouble = 10.0;
+		System.out.println("numeroDouble: " + numeroDouble);
+		// en inferencia de tipos solo tenemos literales para long, float y double.
+	}
+
+}
