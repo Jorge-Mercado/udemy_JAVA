9b8bd9b0312aa7d32408f4369d2b2fcf09d45539
Jorge Mercado <mercadoruizjorgearturo@gmail.com>
2022-04-02 22:46:47 -0600

    primer ejercicio operador ternario

--- src/basico/OperadorTernario.java
+++ src/basico/OperadorTernario.java
@@ -4,6 +4,8 @@ public class OperadorTernario {
 
 	public static void main(String[] args) {
 		
+		var resultado = (1 > 2) ? "verdadero" : "falso" ;
+		System.out.println("resultado: " + resultado);
 
 	}
 
